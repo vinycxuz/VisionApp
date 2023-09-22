4738b43f6e96ee68df6e19008f20b4286f48d1ef
Vinícius Aarão Caldas da Costa <vinyby@gmail.com>
2023-09-21 22:14:29 -0300

    feat(app): create a POST in index.js and test the upload with multer

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
 
+const multer = require('multer')
+const upload = multer({dest: './upload'})
+
+app.set('view engine', 'ejs')
+
 app.get('/ocr', function (req, res) {
-  res.json(console.log('test'))
+  res.render('index')
+})
+
+app.post('/ocr', upload.single('file'), function(req, res) {
+  res.json({message:"successfully"})
 })
 
 app.listen(3000, function() {
